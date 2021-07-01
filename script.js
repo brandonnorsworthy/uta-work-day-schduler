6d2b4a7af2dff8827abffb8ff0df20002d913772
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-07-01 18:24:12 -0500

    fix some formatting slight preferences with save button

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 //html variables
 var currentDayEl = $('#currentDay');
-console.log($('#currentDay'));
+var containerEl = $('#container');
 
 //set moment date
 currentDayEl.text(moment().format("dddd, MMMM Do"));
+
+//create my times
+for (hour = 0; hour < maxHours; hour++){
+
+}
