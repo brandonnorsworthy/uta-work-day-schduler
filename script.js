cf09d44c788adf17d57be0b16662c7a63366eae9
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-07-01 19:13:41 -0500

    working dynamic time picker shows 4 hours in advance and 3 hours back also darkmode

--- script.js
+++ script.js
@@ -1,11 +1,58 @@
 //html variables
 var currentDayEl = $('#currentDay');
-var containerEl = $('#container');
+var containerEl = $('.container');
 
 //set moment date
-currentDayEl.text(moment().format("dddd, MMMM Do"));
 
-//create my times
-for (hour = 0; hour < maxHours; hour++){
-
+function main() {
+    currentDayEl.text(moment().format("dddd, MMMM Do"));
+    var maxHours = 8; //8 hours displayed max 3 previous hours 1 present and 4 future
+    var offsetHours = -3
+    //create my times
+    for (var hour = offsetHours; hour < maxHours + offsetHours; hour++){
+        //div row that holds all the stuffs
+        var timeRowEl = document.createElement('div');
+        timeRowEl.setAttribute('class', 'row time-block');
+    
+        var hourEl = document.createElement('div');
+        hourEl.setAttribute('class', 'col-sm-1 hour');
+        var timeEl = document.createElement('textarea');
+        timeEl.setAttribute('class', 'col-sm-10 description');
+        var saveBtnEl = document.createElement('button');
+        saveBtnEl.setAttribute('class', 'col-sm-1 saveBtn');
+    
+        console.log(`index number: ` + hour)
+        formatMilitaryTime(hour)
+    
+        timeRowEl.append(hourEl);
+        timeRowEl.append(timeEl);
+        timeRowEl.append(saveBtnEl);
+    
+        containerEl.append(timeRowEl);
+    }
 }
+
+function formatMilitaryTime(currentIndex) {
+    var suffix = '';
+    var hour = moment().hour() + currentIndex;
+    
+    console.log(hour)
+
+    if (hour > 24) {
+        hour -= 24;
+        suffix = ' AM';
+    } else if (hour > 12) {
+        suffix = ' PM';
+        hour -= 12;
+    } else if (hour > 0) {
+        suffix = ' AM'
+    } else {
+
+    }
+    hour = hour + suffix;
+
+    console.log(hour.toString())
+    return hour
+}
+
+main();
